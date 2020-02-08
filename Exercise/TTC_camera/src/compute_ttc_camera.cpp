b9d204c3638903d8c70d9027981371c7dd2826bb
Endymion Cheung <endymion.cheung@gmail.com>
2020-02-08 04:20:56 -0500

    Improved robustness computing average of a dataset with outliers
    Computing the average of a dataset with outliers is to use the median instead of mean distance ratio.
    
    Replaced `meanDistRatio` with a variable `medianDistRatio` as well as taking care of the even and an odd number of values in the vector `distRatios`.

--- Exercise/TTC_camera/src/compute_ttc_camera.cpp
+++ Exercise/TTC_camera/src/compute_ttc_camera.cpp
@@ -12,12 +12,12 @@ using namespace std;
 void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr,
                       std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC)
 {
-    // compute distance ratios between all matched keypoints
-    vector<double> distRatios; // stores the distance ratios for all keypoints between curr. and prev. frame
+    // Compute distance ratios between all matched keypoints
+    vector<double> distRatios; // Stores the distance ratios for all keypoints between curr. and prev. frame
     for (auto it1 = kptMatches.begin(); it1 != kptMatches.end() - 1; ++it1)
     { // outer kpt. loop
 
-        // get current keypoint and its matched partner in the prev. frame
+        // Get current keypoint and its matched partner in the prev. frame
         cv::KeyPoint kpOuterCurr = kptsCurr.at(it1->trainIdx);
         cv::KeyPoint kpOuterPrev = kptsPrev.at(it1->queryIdx);
 
@@ -26,37 +26,45 @@ void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPo
 
             double minDist = 100.0; // min. required distance
 
-            // get next keypoint and its matched partner in the prev. frame
+            // Get next keypoint and its matched partner in the prev. frame
             cv::KeyPoint kpInnerCurr = kptsCurr.at(it2->trainIdx);
             cv::KeyPoint kpInnerPrev = kptsPrev.at(it2->queryIdx);
 
-            // compute distances and distance ratios
+            // Compute distances and distance ratios
             double distCurr = cv::norm(kpOuterCurr.pt - kpInnerCurr.pt);
             double distPrev = cv::norm(kpOuterPrev.pt - kpInnerPrev.pt);
 
             if (distPrev > std::numeric_limits<double>::epsilon() && distCurr >= minDist)
-            { // avoid division by zero
+            { // Avoid division by zero
 
                 double distRatio = distCurr / distPrev;
                 distRatios.push_back(distRatio);
             }
-        } // eof inner loop over all matched kpts
-    }     // eof outer loop over all matched kpts
+        } // EOF inner loop over all matched kpts
+    }     // EOF outer loop over all matched kpts
 
-    // only continue if list of distance ratios is not empty
+    // Only continue if list of distance ratios is not empty
     if (distRatios.size() == 0)
     {
         TTC = NAN;
         return;
     }
 
-    // compute camera-based TTC from distance ratios
-    double meanDistRatio = std::accumulate(distRatios.begin(), distRatios.end(), 0.0) / distRatios.size();
+    // Compute camera-based TTC from distance ratios
+    // double meanDistRatio = std::accumulate(distRatios.begin(), distRatios.end(), 0.0) / distRatios.size();
+    // double dT = 1 / frameRate;
+    // TTC = -dT / (1 - meanDistRatio); // Result = 13.0042 s
+
+    // Replaced meanDistRatio with medDistRatio to remove outlier influence
+    // Sort the distance ratio in ascending order
+    std::sort(distRatios.begin(), distRatios.end());
+    // Find the median index
+    long medIndex = floor(distRatios.size() / 2.0);
+    // Compute median distance ratio to remove outlier influence
+    double medDistRatio = distRatios.size() % 2 == 0 ? (distRatios[medIndex - 1] + distRatios[medIndex]) / 2.0 : distRatios[medIndex];
 
     double dT = 1 / frameRate;
-    TTC = -dT / (1 - meanDistRatio);
-
-    // STUDENT TASK (replacement for meanDistRatio)
+    TTC = -dT / (1 - medDistRatio);  // Result = 12.4567 s
 }
 
 int main()
