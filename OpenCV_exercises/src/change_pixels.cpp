34284a7a4ae57d95250a87812c51d4b284ac5c49
Endymion Cheung <endymion.cheung@gmail.com>
2020-02-08 03:56:32 -0500

    Overlayed a white strip in the middle of the image

--- OpenCV_exercises/src/change_pixels.cpp
+++ OpenCV_exercises/src/change_pixels.cpp
@@ -10,15 +10,22 @@ void changePixels()
     // create matrix
     int nrows = 480, ncols = 640;
     cv::Mat m1_8u;
-    m1_8u.create(nrows, ncols, CV_8UC1); // two-channel matrix with 8bit unsigned elements
+    m1_8u.create(nrows, ncols, CV_8UC1); // single-channel matrix with 8bit unsigned elements
     m1_8u.setTo(cv::Scalar(0));          //black
+
+    // cv::Mat m3_8u;
+    // m3_8u.create(nrows, ncols, CV_8UC3); // three-channel matrix with 8bit unsigned elements
+    // m3_8u.setTo(cv::Scalar(0, 0, 0));  // blue
 
     for (int r = 230; r < 250; r++)
     {
         // STUDENT TASK : loop over all columns and set matrix elements to 255
-
+        for (int col = 0; col < m1_8u.cols ; col++)
+            m1_8u.at<unsigned char>(r,col) = 255;
+        // for (int col = 0; col < m3_8u.cols ; col++)
+        //     m3_8u.at<cv::Scalar>(r,col) = cv::Scalar(255, 255, 255);
     }
-
+    
     // show result
     string windowName = "First steps in OpenCV";
     cv::namedWindow(windowName, 1); // create window
