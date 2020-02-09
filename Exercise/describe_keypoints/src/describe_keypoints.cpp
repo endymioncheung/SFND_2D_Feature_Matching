d1306946024031df028c88072395b393e100a533
Endymion Cheung <endymion.cheung@gmail.com>
2020-02-09 06:47:48 -0500

    Implemented the SIFT detector and descriptor
    Implemented Lesson 4: Tracking Image Features - 4. Gradient-based vs. Binary Descriptors - the SIFT detector and descriptor solution code.
    
    Note: In order to run the (patented) SIFT algorithm, it is required to have BOTH:
    - OpenCV libaries that are compiled with the OPENCV_ENABLE_NONFREE CMake option enabled
    - add a line “#include <opencv2/xfeatures2d/nonfree.hpp>” in the .cpp file where the SIFT algorithm is ued
    
    Compared the BRISK and SIFT detector performance:
    BRISK detector with n= 2896 keypoints in 47.437 ms
    BRISK descriptor in 31.4014 ms
    
    SIFT detector with n= 1890 keypoints in 133.894 ms
    SIFT descriptor in 14.383 ms

--- Exercise/describe_keypoints/src/describe_keypoints.cpp
+++ Exercise/describe_keypoints/src/describe_keypoints.cpp
@@ -17,22 +17,34 @@ void descKeypoints1()
     cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
 
     // BRISK detector / descriptor
+    // Other detectors can be also be used:
+    // - BRIEF
+	// - ORB
+	// - FREAK
+    // - KAZE
     cv::Ptr<cv::FeatureDetector> detector = cv::BRISK::create();
+    // Resulting output array (i.e. vector of KeyPoints)
     vector<cv::KeyPoint> kptsBRISK;
 
     double t = (double)cv::getTickCount();
+    // Call BRISK function with grayscale image 
+    // with the result data structure
     detector->detect(imgGray, kptsBRISK);
     t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
     cout << "BRISK detector with n= " << kptsBRISK.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
 
     cv::Ptr<cv::DescriptorExtractor> descriptor = cv::BRISK::create();
+    // Local vicinity of a keypoint stored in a matrix
     cv::Mat descBRISK;
     t = (double)cv::getTickCount();
+    // Execute the descriptor function which takes grayscale image
+    // the keyPoints (can be from other detectors other than BRISK)
     descriptor->compute(imgGray, kptsBRISK, descBRISK);
     t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
     cout << "BRISK descriptor in " << 1000 * t / 1.0 << " ms" << endl;
 
-    // visualize results
+    // Visualize results
+    // Note: This is not the descriptors, it will be visualize in the next lesson
     cv::Mat visImage = img.clone();
     cv::drawKeypoints(img, kptsBRISK, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
     string windowName = "BRISK Results";
@@ -44,7 +56,31 @@ void descKeypoints1()
     // time for both steps and compare both BRISK and SIFT
     // with regard to processing speed and the number and 
     // visual appearance of keypoints.
+    // Visit OpenCV docs https://docs.opencv.org/4.1.1/d5/d3c/classcv_1_1xfeatures2d_1_1SIFT.html
+    // to learn to use SIFT
+    detector = cv::xfeatures2d::SIFT::create();
+    vector<cv::KeyPoint> kptsSIFT;
+    
+    t = (double)cv::getTickCount();
+    detector->detect(imgGray, kptsSIFT);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "SIFT detector with n= " << kptsSIFT.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
 
+    descriptor = cv::xfeatures2d::SIFT::create();
+    // Local vicinity of a keypoint stored in a matrix
+    cv::Mat descSIFT;
+    t = (double)cv::getTickCount();
+    descriptor->compute(imgGray, kptsSIFT, descSIFT);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "SIFT descriptor in " << 1000 * t / 1.0 << " ms" << endl;
+
+    // Visualize results
+    visImage = img.clone();
+    cv::drawKeypoints(img, kptsSIFT, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    windowName = "SIFT Results";
+    cv::namedWindow(windowName, 1);
+    imshow(windowName, visImage);
+    cv::waitKey(0);
 }
 
 int main()
