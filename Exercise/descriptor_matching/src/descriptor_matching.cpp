2ef4dab545593a6ae9f5dfc5dcaa703a306231e0
Endymion Cheung <endymion.cheung@gmail.com>
2020-02-09 23:05:46 -0500

    Implemented the descriptor matching
    Implemented Lesson 4 - Tracking Image Features - 5. Descriptor Matching solution code.
    
    Code contains diferent options of:
    - Dataset selection: small, large BRISK and STIFT detectors/descriptors dataset
    - Cross checking descriptors enabled / disabled
    - Matching method: brute-force (BF), fast library for approximate nearest neighbors (FLANN)
    - Searching nearest neighbors: nearest neighbor, or KNN (i.e. k=2 to choose two best closest descriptors)

--- Exercise/descriptor_matching/src/descriptor_matching.cpp
+++ Exercise/descriptor_matching/src/descriptor_matching.cpp
@@ -13,47 +13,69 @@ void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> 
                       vector<cv::DMatch> &matches, string descriptorType, string matcherType, string selectorType)
 {
 
-    // configure matcher
+    // Configure descriptor matcher
     bool crossCheck = false;
+    // bool crossCheck = true;
     cv::Ptr<cv::DescriptorMatcher> matcher;
 
+    // Brute force matching method
     if (matcherType.compare("MAT_BF") == 0)
     {
-
         int normType = descriptorType.compare("DES_BINARY") == 0 ? cv::NORM_HAMMING : cv::NORM_L2;
         matcher = cv::BFMatcher::create(normType, crossCheck);
         cout << "BF matching cross-check=" << crossCheck;
     }
+    // FLANN method (fast library for approximate nearest neighbors)
     else if (matcherType.compare("MAT_FLANN") == 0)
     {
         if (descSource.type() != CV_32F)
-        { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
+        { 
+            // OpenCV bug workaround : convert binary descriptors to floating point 
+            // due to a bug in current OpenCV implementation
             descSource.convertTo(descSource, CV_32F);
             descRef.convertTo(descRef, CV_32F);
         }
 
         //... TODO : implement FLANN matching
+        matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
         cout << "FLANN matching";
     }
 
-    // perform matching task
+    // Perform matching task
     if (selectorType.compare("SEL_NN") == 0)
-    { // nearest neighbor (best match)
-
+    { 
+        // Nearest neighbor (best match)
         double t = (double)cv::getTickCount();
-        matcher->match(descSource, descRef, matches); // Finds the best match for each descriptor in desc1
+        // Find the best match for each descriptor
+        matcher->match(descSource, descRef, matches);
         t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
         cout << " (NN) with n=" << matches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
     }
     else if (selectorType.compare("SEL_KNN") == 0)
-    { // k nearest neighbors (k=2)
+    { 
+        // k nearest neighbors (k=2)
+        double t = (double)cv::getTickCount();
+        // TODO : Implement k-nearest-neighbor matching with the two best matches
+        vector<vector<cv::DMatch>> knn_matches;
+        matcher->knnMatch(descSource, descRef, knn_matches, 2);
 
-        // TODO : implement k-nearest-neighbor matching
-
-        // TODO : filter matches using descriptor distance ratio test
+        // TODO : Filter matches using descriptor distance ratio test
+        double minDescDistRatio = 0.8;
+        for (auto it = knn_matches.begin(); it != knn_matches.end(); ++it)
+        {
+            // Match found if the descriptor distance ratio of
+            // the source image and reference image is within the threshold
+            if ((*it)[0].distance < minDescDistRatio * (*it)[1].distance)
+            {
+                matches.push_back((*it)[0]);
+            }
+        }
+        cout << "# keypoints removed = " << knn_matches.size() - matches.size() << endl;
+        t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        cout << " (kNN) with n=" << matches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
     }
 
-    // visualize results
+    // Visualize results
     cv::Mat matchImg = imgRef.clone();
     cv::drawMatches(imgSource, kPtsSource, imgRef, kPtsRef, matches,
                     matchImg, cv::Scalar::all(-1), cv::Scalar::all(-1), vector<char>(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
@@ -66,20 +88,44 @@ void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> 
 
 int main()
 {
+    // Load images
     cv::Mat imgSource = cv::imread("../images/img1gray.png");
     cv::Mat imgRef = cv::imread("../images/img2gray.png");
 
-    vector<cv::KeyPoint> kptsSource, kptsRef; 
-    readKeypoints("../dat/C35A5_KptsSource_BRISK_large.dat", kptsSource);
-    readKeypoints("../dat/C35A5_KptsRef_BRISK_large.dat", kptsRef);
+    vector<cv::KeyPoint> kptsSource, kptsRef;
+    cv::Mat descSource, descRef;
+    
+    // Small BRISK dataset
+    readKeypoints("../dat/C35A5_KptsSource_BRISK_small.dat", kptsSource);
+    readKeypoints("../dat/C35A5_KptsRef_BRISK_small.dat", kptsRef);
+    readDescriptors("../dat/C35A5_DescSource_BRISK_small.dat", descSource);
+    readDescriptors("../dat/C35A5_DescRef_BRISK_small.dat", descRef);
 
-    cv::Mat descSource, descRef; 
-    readDescriptors("../dat/C35A5_DescSource_BRISK_large.dat", descSource);
-    readDescriptors("../dat/C35A5_DescRef_BRISK_large.dat", descRef);
+    // Large BRISK dataset
+    // readKeypoints("../dat/C35A5_KptsSource_BRISK_large.dat", kptsSource);
+    // readKeypoints("../dat/C35A5_KptsRef_BRISK_large.dat", kptsRef);
+    // readDescriptors("../dat/C35A5_DescSource_BRISK_large.dat", descSource);
+    // readDescriptors("../dat/C35A5_DescRef_BRISK_large.dat", descRef);
+
+    // SIFT dataset
+    // readKeypoints("../dat/C35A5_KptsSource_SIFT.dat", kptsSource);
+    // readKeypoints("../dat/C35A5_KptsRef_SIFT.dat", kptsRef);
+    // readDescriptors("../dat/C35A5_DescSource_SIFT.dat", descSource);
+    // readDescriptors("../dat/C35A5_DescRef_SIFT.dat", descRef);
 
     vector<cv::DMatch> matches;
-    string matcherType = "MAT_BF"; 
-    string descriptorType = "DES_BINARY"; 
-    string selectorType = "SEL_NN"; 
+
+    // Select brutal force
+    string matcherType = "MAT_BF";
+    // Select fast library for approximate nearest neighbors (FLANN)
+    // string matcherType = "MAT_FLANN";
+    
+    string descriptorType = "DES_BINARY";
+    
+    // Select nearest neighbors
+    string selectorType = "SEL_NN";
+    // Select k nearest neighbors
+    // string selectorType = "SEL_KNN";
+
     matchDescriptors(imgSource, imgRef, kptsSource, kptsRef, descSource, descRef, matches, descriptorType, matcherType, selectorType);
 }
